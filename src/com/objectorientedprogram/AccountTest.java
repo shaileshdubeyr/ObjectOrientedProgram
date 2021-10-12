feb747c0faeeade6b613cff430997e96296a29ec
SHAILESH <shaileshdubeyr@gmail.com>
2021-10-12 19:20:19 +0530

    Refactored : All code sifted in another file

---
+++ src/com/objectorientedprogram/AccountTest.java
@@ -0,0 +1,10 @@
+package com.objectorientedprogram;
+
+public class AccountTest {
+	
+	public static void main(String[] args) {
+		AccountDetails accountObj = new AccountDetails ();
+		accountObj.mainOperation();
+	}
+
+}
